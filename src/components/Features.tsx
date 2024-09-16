624d907bd734f7f750497935097eff2f19bc9d90
amlanxbghn <amlanxconnect@gmail.com>
2024-09-16 18:32:18 +0530

    Hero Skeleton

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -5,18 +5,20 @@ import { FeaturesTable } from "./FeaturesTable";
 
 const Features = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center my-16">
-      <span className="pointer-events-none bg-gradient-to-b from-white to-slate-900/10 bg-clip-text text-center text-5xl md:text-7xl font-semibold text-transparent light:from-black light:to-gray-300/80 mb-8">
-        What We Offer
-      </span>
-      <Particles
-        className="absolute inset-0"
-        quantity={100}
-        ease={80}
-        color="#ffffff"
-        refresh
-      />
-      <FeaturesTable/>
+    <div className="container">
+      <div className="relative flex flex-col items-center justify-center my-16">
+        <span className="pointer-events-none bg-gradient-to-b from-white to-slate-900/10 bg-clip-text text-center text-5xl md:text-7xl font-semibold text-transparent light:from-black light:to-gray-300/80 md:mb-8">
+          What We Offer
+        </span>
+        <Particles
+          className="absolute inset-0"
+          quantity={100}
+          ease={80}
+          color="#ffffff"
+          refresh
+        />
+        <FeaturesTable />
+      </div>
     </div>
   );
 };
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,30 @@
+import { ChevronRightIcon } from "lucide-react"
+import ShimmerButton from "./magicui/shimmer-button"
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="container">
+      <div className="py-24 mt-24 flex flex-col items-center justify-center">
+        <h1 className="text-center bg-gradient-to-br from-white light:from-black from-30% to-white/40 light:to-black/40 bg-clip-text py-6 mx-auto font-semibold leading-none tracking-tighter text-transparent text-balance text-6xl md:text-7xl lg:text-8xl">
+          Crafting Next-Gen<br className="hidden md:block" /> Landing Pages.
+        </h1>
+        <p className="text-center mb-12 text-lg tracking-tight leading-[30px] text-gray-400 md:text-xl text-balance mx-auto">
+          Helping Founders and Early-Stage Startups build High-Conversion<br className="hidden md:block" /> Landing Pages at a Fraction of the Cost.
+        </p>
+
+          <ShimmerButton 
+            className="shadow-2xl mb-8"
+          >
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-[#fff]/75 from-white to-slate-900/10 lg:text-lg">
+              Book a Call
+            </span>
+            <ChevronRightIcon className="ml-1 h-4 w-4 transition-transform duration-300 group-hover:translate-x-1" />
+          </ShimmerButton>
+
+      </div>
+    </div>
   )
 }
 
 export default Hero
+
